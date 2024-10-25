9f25d1b47204d2d70ed885e5c67a72c4bbdd0f97
CoFlores-12 <obethflores2014@gmail.com>
2024-10-24 21:03:49 -0600

    login page init

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
                             <li class="nav-item"><a class="nav-link" href="/">Admissions</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/">Students</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/views/students/login">Students</a></li>
                             <li class="nav-item"><a class="nav-link" href="/">Administration</a></li>
                             
                         </ul>
