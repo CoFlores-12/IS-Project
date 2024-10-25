24202c0a5d088d7f8be7f1d2368bdd240f3ee793
CoFlores-12 <obethflores2014@gmail.com>
2024-10-24 20:15:14 -0600

    add bootstrap & modify landing page

--- index.php
+++ index.php
@@ -1,72 +1,197 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/public/css/theme.css">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>IS Project</title>
+        <link rel="stylesheet" href="/public/css/theme.css">
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <link rel="stylesheet" href="/public/bootstrap-5.3.3-dist/css/bootstrap.min.css">
 
-    <style>
-        .header{
-            background-color: var(--primary-color);
-            height: 70%;
-        }
-        .banner {
-            padding-top: 10%;
-        }
-        .svg {
-            fill: var(--primary-color);
-            position: relative;
-            z-index: -1;
-        }
-        .active {
-            color: var(--accent-color) !important;
-        }
-        main { 
-            height: 100%;
-        }
-        .nav {
-            position: fixed;
-            width: 100%;
-            box-sizing: border-box;
-            z-index: 9999;
-            background-color: var(--primary-color);
-        }
-      
-    </style>
-</head>
-<body>
-    <main>
-        <div class="nav p-4 flex justify-between items-center">
-            <a class="text-3xl font-bold text-white" href="/">
-                <!-- <img class="h-9" src="logo.png" alt="logo"> -->
-                Logo
-            </a>
-            <div class="flex-1 justify-center items-center flex">
-                <a class="text-white active" href="/">Home</a>
-                <a class="text-white space-x-12" href="/">Admissions</a>
-                <a class="text-white space-x-12" href="/">Students</a>
-                <a class="text-white space-x-12" href="/">Administration</a>
-            </div>
-            <a class="text-white px-4 py-2 bg-blue font-bold rounded" href="#">
-                Login
-            </a>
-        </div>
+        <style>            
+            .feature {
+                display: inline-flex;
+                align-items: center;
+                justify-content: center;
+                height: 3rem;
+                width: 3rem;
+                font-size: 1.5rem;
+            }
 
-        <div class="header relative z-10">
-            <div class="banner z-10 flex justify-between p-4 box-sizing-border">
-                <div class="info text-center flex-1">
-                    <h1>Started</h1>
+            .bg-featured-blog {
+                height: 100%;
+                width: 100%;
+                background-size: cover;
+                background-position: center;
+                background-repeat: no-repeat;
+                min-height: 15rem;
+            }
+            .card {
+                background-color: var(--bg-aux) !important;
+                color: var(--text) !important;
+            }
+        </style>
+    </head>
+    <body class="d-flex flex-column h-100">
+        <main class="flex-shrink-0">
+            <!-- Navigation-->
+            <nav class="navbar navbar-expand-lg">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="index.html">LOGO</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/">Admissions</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/">Students</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/">Administration</a></li>
+                            
+                        </ul>
+                    </div>
                 </div>
-                <div class="flex-1 flex justify-content">
-                    <img width="90%" class="max-w-90" src="/public/images/edu.png" alt="">
+            </nav>
+            <!-- Header-->
+            <header class=" py-5">
+                <div class="container px-5">
+                    <div class="row gx-5 align-items-center justify-content-center">
+                        <div class="col-lg-8 col-xl-7 col-xxl-6">
+                            <div class="my-5 text-center text-xl-start">
+                                <h1 class="display-5 fw-bolder mb-2">Transform your Future with Quality Education</h1>
+                                <p class="lead fw-normal  mb-4">Discover academic programs of excellence, expert teachers, and a community committed to your success.</p>
+                                <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                                    <a class="btn btn-primary text-inverte btn-lg px-4 me-sm-3" href="/">Get Started</a>
+                                    <a class="btn btn-outline btn-lg px-4" href="#features">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
+                            <img class="img-fluid rounded-3 my-5" src="/public/images/edu.png" alt="..." /></div>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path class="svg" fill-opacity="1" d="M0,96L80,96C160,96,320,96,480,122.7C640,149,800,203,960,192C1120,181,1280,107,1360,69.3L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
-        <div class="h-full">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos recusandae doloribus totam quis laudantium earum rem molestiae, voluptate blanditiis alias! Laudantium nobis ducimus, consequuntur ut suscipit dicta quam cupiditate doloremque.</div>
-        <div class="h-full">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore sequi eius perferendis dolorum iusto sapiente, modi magni eveniet nesciunt alias, ipsum amet praesentium quidem nostrum et consectetur expedita! Vitae, eius.</div>
-        <div class="h-full"></div>
+            </header>
+            <!-- Features section-->
+            <section class="py-5" id="features">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5">
+                        <div class="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0">A better way to start building.</h2></div>
+                        <div class="col-lg-8">
+                            <div class="row gx-5 row-cols-1 row-cols-md-2">
+                                <div class="col mb-5 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
+                                    <h2 class="h5">Featured title</h2>
+                                    <p class="mb-0 text-secundary">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                                <div class="col mb-5 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
+                                    <h2 class="h5">Featured title</h2>
+                                    <p class="mb-0 text-secundary">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                                <div class="col mb-5 mb-md-0 h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
+                                    <h2 class="h5">Featured title</h2>
+                                    <p class="mb-0 text-secundary">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                                <div class="col h-100">
+                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
+                                    <h2 class="h5">Featured title</h2>
+                                    <p class="mb-0 text-secundary">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Blog preview section-->
+            <section class="py-5">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5 justify-content-center">
+                        <div class="col-lg-8 col-xl-6">
+                            <div class="text-center">
+                                <h2 class="fw-bolder">From our blog</h2>
+                                <p class="lead fw-normal text-secundary mb-5 text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row gx-5">
+                        <div class="col-lg-4 mb-5">
+                            <div class="card h-100 shadow border-0">
+                                <img class="card-img-top" src="https://dummyimage.com/600x350/ced4da/6c757d" alt="..." />
+                                <div class="card-body p-4">
+                                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title text mb-3">Blog post title</h5></a>
+                                    <p class="card-text mb-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                                </div>
+                                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                                    <div class="d-flex align-items-end justify-content-between">
+                                        <div class="d-flex align-items-center">
+                                            <div class="small">
+                                                <div class="text-secundary">Oct 22, 2024</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-5">
+                            <div class="card h-100 shadow border-0">
+                                <img class="card-img-top" src="https://dummyimage.com/600x350/adb5bd/495057" alt="..." />
+                                <div class="card-body p-4">
+                                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title text mb-3">Another blog post title</h5></a>
+                                    <p class="card-text mb-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                                </div>
+                                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                                    <div class="d-flex align-items-end justify-content-between">
+                                        <div class="d-flex align-items-center">
+                                            <div class="small">
+                                                <div class="text-secundary">Oct 23, 2024</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 mb-5">
+                            <div class="card h-100 shadow border-0">
+                                <img class="card-img-top" src="https://dummyimage.com/600x350/6c757d/343a40" alt="..." />
+                                <div class="card-body p-4">
+                                    <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title text mb-3">The last blog post title</h5></a>
+                                    <p class="card-text mb-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque fugit ratione dicta mollitia. Officiis ad.</p>
+                                </div>
+                                <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+                                    <div class="d-flex align-items-end justify-content-between">
+                                        <div class="d-flex align-items-center">
+                                            <div class="small">
+                                                <div class="text-secundary">Oct 25, 2023</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
-</body>
+        <!-- Footer-->
+        <footer class=" py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; IS Project 2024</div></div>
+                    <div class="col-auto">
+                        <a class="link-light small" href="#!">Privacy</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="#!">Terms</a>
+                        <span class="text-white mx-1">&middot;</span>
+                        <a class="link-light small" href="#!">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="/public/bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
+    </body>
 </html>
