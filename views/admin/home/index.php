8a178547b2c5ccc4365b01859a9aea4889b5bf4f
CoFlores-12 <obethflores2014@gmail.com>
2024-10-26 19:05:58 -0600

    new user form & user table

--- views/admin/home/index.php
+++ views/admin/home/index.php
@@ -10,6 +10,72 @@
     <link rel="stylesheet" href="/public/bootstrap-5.3.3-dist/css/bootstrap.min.css">
 </head>
 <body>
+
+<!-- Modal New User -->
+<div class="modal fade" id="newUserModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content bg">
+      <div class="modal-header bg">
+        <h5 class="modal-title text" id="staticBackdropLabel">New User</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form class="needs-validation bg rounded p-4" novalidate method="POST" action="/api/post/admin/addUser.php"  enctype="multipart/form-data">
+            <input type="text" name="role" hidden readonly value="Teacher">
+            <div class="row mb-4">
+                <div class="form-group">
+                    <input name="employeeNumber" type="text" class="form-control" id="employeeNumber" placeholder="Employee Number" maxlength="15" required>
+                </div>
+            </div>
+            <div class="form-row flex gap-4">
+                <div class="col mb-3">
+                    <input name="name" type="text" class="form-control" id="validationCustom01" placeholder="First name"  required>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
+                </div>
+                <div class="col mb-3">
+                    <input name="lastName" type="text" class="form-control" id="validationCustom02" placeholder="Last name" required>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-4">
+                <div class="form-group">
+                    <input name="identity" type="text" class="form-control" id="identity" placeholder="identity" maxlength="15" required>
+                </div>
+            </div>
+            <div class="form-row flex gap-4">
+                <div class="col mb-3">
+                    <input name="phone" type="text" class="form-control" id="phone" placeholder="Phone Number" maxlength="9" required>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
+                </div>
+                <div class="col mb-3">
+                    <input name="email" type="email" class="form-control" id="email" placeholder="Email" required>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" class="btn bg-custom-primary text-white">Save</button>
+        </div>
+    </form>
+    </div>
+  </div>
+</div>
+<!-- Modal New User -->
+
+
+
+
+
+
 <div class="main">
         <div class="offcanvas offcanvas-start bg" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
             <div class="offcanvas-header justify-between">
@@ -48,7 +114,7 @@
             <div class=" flex p-2 justify-between items-center">
                 <h4 class="text">Dashboard</h4>
                 <div class="vuttons">
-                    <button class="btn bg-custom-primary text-white">Add User</button>
+                    <button class="btn bg-custom-primary text-white"  data-bs-toggle="modal" data-bs-target="#newUserModal">Add User</button>
                 </div>
             </div>
             <div class="row p-4">
@@ -82,9 +148,14 @@
                 <div class="card rounded p-2">
                     <div class="card-header flex justify-between items-center">
                         <span>Admissions <span class="text-xxs text-secondary">(26/10/2024 - 30/10/2024)</span></span>
-                        <a class="btn text-success btn-outline-success" href="/api/get/admin/admittedStudents.php">
-                            Export <i class="bi bi-arrow-bar-up"></i>
-                        </a>
+                        <div class="actions">
+                            <button class="btn text-primary btn-outline-primary">
+                                Import scores <i class="bi bi-arrow-bar-down"></i>
+                            </button>
+                            <a class="btn text-success btn-outline-success" href="/api/get/admin/admittedStudents.php">
+                                Export Admitteds <i class="bi bi-arrow-bar-up"></i>
+                            </a>
+                        </div>
                     </div>
                     <div class="card-body">
                         List
@@ -94,5 +165,24 @@
         </div>
     </div>
     <script src="/public/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        var forms = document.getElementsByClassName('needs-validation');
+        var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+        });
+    }, false);
+    })();
+
+    
+    </script>
 </body>
 </html>
