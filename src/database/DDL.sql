ccbe7c413ac8183657329dcd7dbc154c3ae85ebd
CoFlores-12 <obethflores2014@gmail.com>
2024-11-07 20:01:57 -0600

    DATABASE CHANGE

--- src/database/DDL.sql
+++ src/database/DDL.sql
@@ -1,5 +1,7 @@
 SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE StatusApplicant;
 DROP TABLE `Applicant`;
+DROP TABLE Roles;
 DROP TABLE `Applicant_result`;
 DROP TABLE `Careers`;
 DROP TABLE `Classes`;
@@ -12,7 +14,6 @@ DROP TABLE `Regional_center`;
 DROP TABLE `Students`;
 DROP TABLE `Administrators`;
 SET FOREIGN_KEY_CHECKS = 1;
-
 
 CREATE TABLE Regional_center (
     center_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -34,6 +35,12 @@ CREATE TABLE Faculty (
     faculty_name VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE CareersXRegionalCenter (
+    career_id INT,
+    center_id INT,
+    Foreign Key (career_id) REFERENCES Careers(career_id),
+    Foreign Key (center_id) REFERENCES Regional_center(center_id)
+);
 CREATE TABLE Careers (
     career_id INT PRIMARY KEY AUTO_INCREMENT,
     career_name VARCHAR(100) NOT NULL,
@@ -72,6 +79,11 @@ CREATE TABLE ClassesXCareer (
     FOREIGN KEY (career_id) REFERENCES Careers(career_id)
 );
 
+CREATE TABLE StatusApplicant (
+    status_id TINYINT PRIMARY KEY
+    description VARCHAR(20)
+)
+
 CREATE TABLE Applicant (
     applicant_id INT PRIMARY KEY AUTO_INCREMENT,
     person_id VARCHAR(20), -- (identity_number)
@@ -79,17 +91,20 @@ CREATE TABLE Applicant (
     secondary_career_id INT,
     certify VARCHAR(255),
     inscription_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(50),
+    status_id TINYINT,
+    counter TINYINT DEFAULT 1,
     FOREIGN KEY (person_id) REFERENCES Persons(person_id),
     FOREIGN KEY (preferend_career_id) REFERENCES Careers(career_id),
-    FOREIGN KEY (secondary_career_id) REFERENCES Careers(career_id)
+    FOREIGN KEY (secondary_career_id) REFERENCES Careers(career_id),
+    Foreign Key (status_id) REFERENCES StatusApplicant(Status_id)
 );
 
 CREATE TABLE Applicant_result (
     result_id INT PRIMARY KEY AUTO_INCREMENT,
     identity_number VARCHAR(20),
-    result FLOAT,
     exam_code VARCHAR(20),
+    result_exam FLOAT,
+    obs BIT --(1: passed, 0: repro)
     FOREIGN KEY (identity_number) REFERENCES Persons(person_id),
     FOREIGN KEY (exam_code) REFERENCES Exams(exam_code)
 );
@@ -97,7 +112,7 @@ CREATE TABLE Applicant_result (
 CREATE TABLE Students (
     account_number VARCHAR(11) NOT NULL  PRIMARY KEY,
     person_id VARCHAR(20), -- (identity_number)
-    password VARCHAR(255) NOT NULL, 
+    password VARBINARY(255) NOT NULL, 
     institute_email VARCHAR(100) UNIQUE,
     direction VARCHAR(255),
     photos JSON,
@@ -106,11 +121,86 @@ CREATE TABLE Students (
     FOREIGN KEY (career_id) REFERENCES Careers(career_id)
 );
 
+CREATE TABLE Roles (
+    role_id TINYINT PRIMARY KEY,
+    type VARCHAR(20) --(jefe, coordinador, docente, etc)
+    route VARCHAR(20) --(admin, teacher, etc)
+)
 CREATE TABLE Administrators (
     employee_number INT PRIMARY KEY AUTO_INCREMENT,
     person_id VARCHAR(20),
-    role VARCHAR(25),
-    password VARCHAR(255) NOT NULL, 
+    role_id TINYINT,
+    password VARBINARY(255) NOT NULL, 
     institute_email VARCHAR(100) UNIQUE,
-    FOREIGN KEY (person_id) REFERENCES Persons(person_id)
+    FOREIGN KEY (person_id) REFERENCES Persons(person_id),
+    Foreign Key (role) REFERENCES Roles(role_id)
 )
+
+CREATE TABLE Config (
+    config_id BINARY PRIMARY KEY AUTO_INCREMENT,
+    data JSON
+)
+
+DELIMITER //
+
+CREATE PROCEDURE CreateAdministrator (
+    IN in_person_id VARCHAR(20),
+    IN in_role VARCHAR(25),
+    IN in_password VARCHAR(255),
+    IN in_institute_email VARCHAR(100)
+)
+BEGIN
+    DECLARE secret_key VARCHAR(255); 
+
+    SELECT JSON_UNQUOTE(JSON_EXTRACT(data, '$.phraseEncrypt'))
+    INTO secret_key
+    FROM Config
+    WHERE config_id = 1;
+
+    START TRANSACTION;
+
+    INSERT INTO Administrators (person_id, role, password, institute_email)
+    VALUES (
+        in_person_id,
+        in_role,
+        AES_ENCRYPT(in_password, secret_key),
+        in_institute_email
+    );
+
+    COMMIT;
+END //
+
+DELIMITER //
+
+CREATE PROCEDURE LoginAdministrator (
+    IN in_identifier VARCHAR(100),      
+    IN in_password VARCHAR(255),       
+    OUT is_authenticated BOOLEAN,       
+    OUT out_role VARCHAR(25)            
+)
+BEGIN
+    DECLARE secret_key VARCHAR(255);     
+    DECLARE db_password VARBINARY(255);   
+
+    SET is_authenticated = FALSE;
+    SET out_role = '0';
+
+    SELECT JSON_UNQUOTE(JSON_EXTRACT(data, '$.phraseEncrypt'))
+    INTO secret_key
+    FROM Config
+    WHERE config_id = 1;
+
+    SELECT password, role INTO db_password, out_role
+    FROM Administrators
+    WHERE (institute_email = in_identifier OR employee_number = in_identifier);
+
+    IF db_password IS NOT NULL AND AES_DECRYPT(db_password, secret_key) = in_password THEN
+        SET is_authenticated = TRUE;
+    ELSE
+        SET out_role = '0'; 
+    END IF;
+END //
+
+DELIMITER ;
+
+
