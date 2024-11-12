9ac69ecdf50860f9d67a65e1d0d0299ddb8d4350
Cesar Flores <48534199+CoFlores-12@users.noreply.github.com>
2024-11-12 10:32:37 -0600

    Update sessionValidation.php

--- src/components/sessionValidation.php
+++ src/components/sessionValidation.php
@@ -1,7 +1,9 @@
 <?php
 
 if (!isset($_SESSION['route']) || 
-    !isset($_SESSION['request']) || 
-    $_SESSION['route'] != $_SESSION['request']) {
+    !isset($_SESSION['request'])) {
+        header('Location: /api/get/logout.php');
+}
+if ($_SESSION['route'] != $_SESSION['request']) {
         header('Location: /api/get/logout.php');
 }
