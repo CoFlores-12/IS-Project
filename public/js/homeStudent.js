1f3fd4071c33f306a66242fa5c45fe43b3398dca
Daniel Avila <daniel.avila.a.v.2000@gmail.com>
2024-11-20 14:07:25 -0300

    Guardar cambios en homeStudent.js antes de cambiar de rama

--- public/js/homeStudent.js
+++ public/js/homeStudent.js
@@ -34,14 +34,14 @@ let optionsBody = async (value) => {
                 if (data.status === true) {
                     body += '<input type="file" name="evidemce"  accept="application/pdf" class="form-control my-4" id="evidence">';
                 } else {
-                    body += `<div class="alert alert-danger mt-3" role="alert">
+                    body = `<div class="alert alert-danger mt-3" role="alert">
                                 Inactive period
                             </div>`;
                 }
                 
             } catch (error) {
                 console.error('Error:', error);
-                body += `<div class="alert alert-danger mt-3" role="alert">
+                body = `<div class="alert alert-danger mt-3" role="alert">
                             Error while fetching data
                         </div>`;
             }
