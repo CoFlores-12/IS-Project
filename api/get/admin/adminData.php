40da375b3fd310fe6698344a67bf0444ecdf0165
CoFlores-12 <obethflores2014@gmail.com>
2024-11-23 11:38:49 -0600

    fix duplicate

--- api/get/admin/adminData.php
+++ api/get/admin/adminData.php
@@ -27,44 +27,6 @@ $result = $db->execute_query("SELECT JSON_UNQUOTE(JSON_EXTRACT(data, '$.registra
         WHERE config_id = 1;");
 $response['registrationPeriod'] = json_decode($result->fetch_assoc()['registrationPeriod']);
 
-
-$result = $db->execute_query("SELECT JSON_UNQUOTE(JSON_EXTRACT(data, '$.cancellationExceptional')) AS cancellationExceptional 
-          FROM Config 
-          WHERE config_id = 1;");
-$response['cancellationExceptional'] = json_decode($result->fetch_assoc()['cancellationExceptional']);
-
-
-
-echo json_encode($response);
-<?php
-
-require_once '../../../src/modules/Auth.php';
-
-$requiredRole = 'Administrator';
-
-AuthMiddleware::checkAccess($requiredRole);
-include '../../../src/modules/database.php';
-
-$db = (new Database())->getConnection();
-$response = [];
-
-$result = $db->execute_query("SELECT COUNT(*) AS count FROM `Employees` WHERE role_id IN (3,4,5)");
-$response['Teachers'] = $result->fetch_assoc()['count'];
-
-$result = $db->execute_query("SELECT COUNT(*) AS count FROM Students");
-$response['Students'] = $result->fetch_assoc()['count'];
-
-$result = $db->execute_query("SELECT COUNT(*) AS count FROM Careers");
-$response['Careers'] = $result->fetch_assoc()['count'];
-
-$result = $db->execute_query("SELECT COUNT(*) AS count FROM Regional_center");
-$response['Regional_center'] = $result->fetch_assoc()['count'];
-
-$result = $db->execute_query("SELECT JSON_UNQUOTE(JSON_EXTRACT(data, '$.registrationPeriod')) as registrationPeriod
-        FROM Config
-        WHERE config_id = 1;");
-$response['registrationPeriod'] = json_decode($result->fetch_assoc()['registrationPeriod']);
-
 $result = $db->execute_query("SELECT log_id, 
        CONVERT_TZ(DATE, '+00:00', '-06:00') AS local_time, 
        ip_address, 
@@ -81,5 +43,12 @@ if ($result) {
     }
 }
 $response['logs'] = $resultArray;
+
+$result = $db->execute_query("SELECT JSON_UNQUOTE(JSON_EXTRACT(data, '$.cancellationExceptional')) AS cancellationExceptional 
+          FROM Config 
+          WHERE config_id = 1;");
+$response['cancellationExceptional'] = json_decode($result->fetch_assoc()['cancellationExceptional']);
+
+
 
 echo json_encode($response);
